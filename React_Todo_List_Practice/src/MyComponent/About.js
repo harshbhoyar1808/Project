40db00c61c8bba7c711546ca0239427cec44d620
harshbhoyar1808 <69676512+harshbhoyar1808@users.noreply.github.com>
2022-03-17 01:19:26 +0530

    Add files via upload

---
+++ React_Todo_List_Practice/src/MyComponent/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      This is a about component
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio maxime ullam consectetur? Sint praesentium facilis voluptates aliquid neque quisquam sed voluptatem dignissimos alias ullam fuga porro deleniti, quidem veritatis minima quos consectetur quasi qui.</p>
+    </div>
+  );
+}
+
+export default About;
